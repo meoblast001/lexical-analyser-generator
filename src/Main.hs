d89675a84750005d7c4c789bbc2bb405ecd676c4
Braden Walters <vc@braden-walters.info>
2015-02-22 13:17:19 -0500

    Current default program mode invoked through "info" argument

--- src/Main.hs
+++ src/Main.hs
@@ -15,35 +15,35 @@ main :: IO ()
 main = do
   args <- getArgs
   case args of
-    [input] -> process input
+    ["info", input] -> displayInfo input
     _ -> error "Please provide an input file name."
 
-process :: FilePath -> IO ()
-process filename = do
+displayInfo :: FilePath -> IO ()
+displayInfo filename = do
   fileHandle <- openFile filename ReadMode
   contents <- hGetContents fileHandle
-  either (putStrLn . show) processRules (parse contents)
+  either (putStrLn . show) displayRulesInfo (parse contents)
 
-processRules :: [Rule] -> IO ()
-processRules rules = do
+displayRulesInfo :: [Rule] -> IO ()
+displayRulesInfo rules = do
   putStrLn "Rules:"
   mapM_ (putStrLn . show) rules
   putStrLn ""
-  mapM_ printRegexTable rules
+  mapM_ printRegexTables rules
 
-printRegexTable :: Rule -> IO ()
-printRegexTable rule@(Token name rx) = do
+printRegexTables :: Rule -> IO ()
+printRegexTables rule@(Token name rx) = do
   let seTable = buildStartEndTable rx
       followTable = buildFollowTable seTable
   putStrLn ("Start-end table for \"" ++ name ++ "\":")
   putStrLn $ show seTable
   putStrLn ("Follow table for \"" ++ name ++ "\":")
   putStrLn $ show followTable
-printRegexTable ignore@(Ignore rx) = do
+printRegexTables ignore@(Ignore rx) = do
   let seTable = buildStartEndTable rx
       followTable = buildFollowTable seTable
   putStrLn "Start-end table for ignore:"
   putStrLn $ show seTable
   putStrLn "Follow table for ignore:"
   putStrLn $ show followTable
-printRegexTable _ = do return ()
+printRegexTables _ = do return ()
